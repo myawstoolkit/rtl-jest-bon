0f2d3a4e96b3dd93b7b7c01e0ebfb393a388135e
myawstoolkit <myawstoolkit@gmail.com>
2022-09-30 15:52:41 +0800

    Test and code checkbox functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import { useState } from "react";
+import "./App.css";
 
 function App() {
 	const [btnColor, setBtnColor] = useState("red");
+	const [isDisabled, setDisabled] = useState(false);
 	let newColor = btnColor === "red" ? "blue" : "red";
-
-	const handleClick = () => {
-		setBtnColor(newColor);
-	};
 
 	return (
 		<div>
-			<button style={{ backgroundColor: btnColor, color: "white" }} onClick={handleClick}>
+			<button
+				style={{ backgroundColor: btnColor, color: "white" }}
+				onClick={() => setBtnColor(newColor)}
+				disabled={isDisabled}>
 				{btnColor === "red" ? "Change to blue" : "Change to red"}
 			</button>
+			<hr />
+			<input
+				type="checkbox"
+				id="enable-button-checkbox"
+				aria-checked={isDisabled}
+				defaultChecked={isDisabled}
+				checked={isDisabled}
+				onChange={(e) => setDisabled(e.target.checked)}
+			/>
 		</div>
 	);
 }
--- src/App.test.js
+++ src/App.test.js
@@ -10,3 +10,26 @@ test("button has correct initial red color", () => {
 	expect(colorButton.textContent).toBe("Change to red");
 	expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
 });
+
+
+test("initial conditions", () => {
+	render(<App />);
+
+	const colorButton = screen.getByRole("button", { name: "Change to blue" });
+	expect(colorButton).toBeEnabled();
+
+	const checkbox = screen.getByRole("checkbox");
+	expect(checkbox).not.toBeChecked();
+});
+
+test("if checkbox toggles button disable/enable condition", () => {
+	render(<App />);
+	const colorButton = screen.getByRole("button");
+	const checkbox = screen.getByRole("checkbox");
+
+	fireEvent.click(checkbox);
+	expect(colorButton).toBeDisabled();
+
+	fireEvent.click(checkbox);
+	expect(colorButton).not.toBeDisabled();
+});
